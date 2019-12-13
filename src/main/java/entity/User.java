d7b2ef631def9702b8e64d041a5258cd26e865ef
anar0508 <anar_ismayilov@ymail.com>
2019-12-13 11:00:34 +0400

    Test_database was created

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,51 @@
 package entity;
 
 public class User {
+    private final int id;
+    private final String surname;
+    private final String name;
+    private final String email;
+    private final String password;
+    private final String picUrl;
+//    private EmptyPhoto empty_pic;
+
+
+    public User(int id, String name, String surname, String email, String password, String picUrl) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.picUrl = picUrl;
+    }
+
+    public User(int id, String name, String surname, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.picUrl = "https://icon-library.net/images/no-profile-pic-icon/no-profile-pic-icon-5.jpg";
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPicUrl() {
+        return picUrl;
+    }
 }
+
---
+++ src/main/java/repo/UsersRepository.java
@@ -0,0 +1,11 @@
+package repo;
+
+import entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<User, Long> {
+
+
+
+
+}
